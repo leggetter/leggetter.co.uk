1776ac82e89ad06f6170255a8ed8829f5a9c15c7
leggetter <phil@leggetter.co.uk>
2015-08-22 14:49:51 +0100

    Update 2015-08-22T13:49:37.445Z

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,13 @@ var run = require('gulp-run');
 var ghPages = require('gulp-gh-pages');
 var webserver = require('gulp-webserver');
 
-gulp.task('default', ['build'], function() {
+function copyImages() {
   gulp.src('./_includes/realtime-web-technologies-guide/images/**/*')
     .pipe(gulp.dest('./_site/real-time-web-technologies-guide/images'));
+}
+
+gulp.task('default', ['build'], function() {
+  copyImages();
   gulp.src('_site')
     .pipe(webserver({
       livereload: true,
@@ -14,16 +18,12 @@ gulp.task('default', ['build'], function() {
     }));
 });
 
-gulp.task('copy-images', function() {
-  return gulp.src('./_includes/realtime-web-technologies-guide/images/**/*')
-    .pipe(gulp.dest('./_site/real-time-web-technologies-guide/images'));
-});
-
 gulp.task('build', function() {
   return run('jekyll build').exec();
 });
 
-gulp.task('deploy', ['build', 'copy-images'], function() {
+gulp.task('deploy', ['build'], function() {
+  copyImages();
   return gulp.src('./_site/**/*')
     .pipe(ghPages());
 });

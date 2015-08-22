4b1cd5783888385023fb35221ca8d37e226c31ea
leggetter <phil@leggetter.co.uk>
2015-08-22 16:36:18 +0100

    Improved build

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp = require('gulp');
+var gulpsync = require('gulp-sync')(gulp);
 var run = require('gulp-run');
 var ghPages = require('gulp-gh-pages');
 var webserver = require('gulp-webserver');
 
-gulp.task('default', ['build'], function() {
-  gulp.src('./_includes/realtime-web-technologies-guide/images/**/*')
-    .pipe(gulp.dest('./_site/real-time-web-technologies-guide/images'));
-  gulp.src('_site')
-    .pipe(webserver({
+gulp.task('server', function() {
+  return gulp.src('_site')
+  .pipe(
+    webserver({
       livereload: true,
       open: true,
       port: 8080
-    }));
+    })
+  );
 });
 
 gulp.task('copy-images', function() {
@@ -23,7 +24,25 @@ gulp.task('build', function() {
   return run('jekyll build').exec();
 });
 
-gulp.task('deploy', ['build', 'copy-images'], function() {
+gulp.task('gh-pages', function() {
   return gulp.src('./_site/**/*')
-    .pipe(ghPages());
+    .pipe(ghPages());  
 });
+
+// Main tasks
+
+gulp.task('deploy', gulpsync.sync(
+  [
+    'build',
+    'copy-images',
+    'gh-pages'
+  ]
+));
+
+gulp.task('default', gulpsync.sync(
+  [
+    'build',
+    'copy-images',
+    'server'
+  ]
+));

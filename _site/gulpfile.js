8ac42018be5970e733a2067ac4a0a95ba4e3d0e7
Phil Leggetter <phil@leggetter.co.uk>
2022-10-02 19:32:43 +0100

    chore: trying deployment without Gulp

---
+++ _site/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var gulpsync = require('gulp-sync')(gulp);
+var run = require('gulp-run');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('copy-images', function() {
+  return gulp.src('./_includes/realtime-web-technologies-guide/images/**/*')
+    .pipe(gulp.dest('./_site/real-time-web-technologies-guide/images'));
+});
+
+gulp.task('build', function() {
+  return run('bundle exec jekyll build').exec();
+});
+
+gulp.task('gh-pages', function() {
+  return gulp.src('./_site/**/*')
+    .pipe(ghPages());  
+});
+
+// Main tasks
+
+gulp.task('deploy', gulpsync.sync(
+  [
+    'build',
+    'copy-images',
+    'gh-pages'
+  ]
+));
+
+gulp.task('default', gulpsync.sync(
+  [
+    'build',
+    'copy-images'
+  ]
+));
